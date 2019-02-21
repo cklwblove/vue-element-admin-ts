3bd1d0ae8e65a20ab90445ecb5a2d63cde8b82c0
cklwblove <lwbhtml@163.com>
2019-02-21 19:07:52 +0800

    feat:

--- src/components/Share/DropdownMenu.vue
+++ src/components/Share/DropdownMenu.vue
@@ -27,5 +27,77 @@ export default class DropdownMenu extends Vue {
 </script>
 
 <style rel="stylesheet/less" lang="less" scoped>
+  @n: 8; //和items.length 相同
+  @t: .1s;
+  .share-dropdown-menu {
+    width: 250px;
+    position: relative;
+    z-index: 1;
 
+    &-title {
+      width: 100%;
+      display: block;
+      cursor: pointer;
+      background: black;
+      color: white;
+      height: 60px;
+      line-height: 60px;
+      font-size: 20px;
+      text-align: center;
+      z-index: 2;
+      transform: translate3d(0, 0, 0);
+    }
+
+    &-wrapper {
+      position: relative;
+    }
+
+    &-item {
+      text-align: center;
+      position: absolute;
+      width: 100%;
+      background: #e0e0e0;
+      line-height: 60px;
+      height: 60px;
+      cursor: pointer;
+      font-size: 20px;
+      opacity: 1;
+      transition: transform 0.28s ease;
+
+      &:hover {
+        background: black;
+        color: white;
+      }
+
+      .loop(@i) when (@i > 0) {
+        &:nth-of-type(@{i}) {
+          z-index: -1;
+          transition-delay: @i*@t;
+          transform: translate3d(0, -60px, 0);
+        }
+        // 循环调用自身
+        .loop((@i - 1));
+      }
+      .loop(@n);
+    }
+
+    &.active {
+      .share-dropdown-menu-wrapper {
+        z-index: 1;
+      }
+
+      .share-dropdown-menu-item {
+        .loop(@i) when (@i > 0) {
+          &:nth-of-type(@{i}) {
+            z-index: -1;
+            transition-delay: (@n - @i)*@t;
+            transform: translate3d(0, (@i - 1) * 60px, 0);
+          }
+          // 循环调用自身
+          .loop((@i - 1));
+        }
+        .loop(@n);
+      }
+    }
+  }
 </style>
