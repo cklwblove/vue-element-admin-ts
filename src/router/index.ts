c67a4870838158b872c94631532498d4e2bd70e5
cklwblove <lwbhtml@163.com>
2019-02-25 18:03:49 +0800

    feat: icon,errorLog 等页面改造

--- src/router/index.ts
+++ src/router/index.ts
@@ -138,6 +138,55 @@ export const asyncRouterMap = [
       }
     ]
   },
+  {
+    path: '/icon',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        component: loadView('svgIcons'),
+        name: 'Icons',
+        meta: { title: 'icons', icon: 'icon', noCache: true }
+      }
+    ]
+  },
+  {
+    path: '/error',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'ErrorPages',
+    meta: {
+      title: 'errorPages',
+      icon: '404'
+    },
+    children: [
+      {
+        path: '401',
+        component: () => import('@/views/errorPage/401.vue'),
+        name: 'Page401',
+        meta: { title: 'page401', noCache: true }
+      },
+      {
+        path: '404',
+        component: () => import('@/views/errorPage/404.vue'),
+        name: 'Page404',
+        meta: { title: 'page404', noCache: true }
+      }
+    ]
+  },
+  {
+    path: '/error-log',
+    component: Layout,
+    redirect: 'noredirect',
+    children: [
+      {
+        path: 'log',
+        component: loadView('errorLog'),
+        name: 'ErrorLog',
+        meta: { title: 'errorLog', icon: 'bug' }
+      }
+    ]
+  },
   { path: '*', redirect: '/404', hidden: true }
 ];
 
---
+++ src/views/errorLog/errorTestB.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class ErrorTestB extends Vue {
+    public created() {
+      // @ts-ignore
+      this.b = b; // eslint-disable-line
+    }
+  }
+</script>
+
+<style rel="stylesheet/less" lang="less" scoped>
+
+</style>
