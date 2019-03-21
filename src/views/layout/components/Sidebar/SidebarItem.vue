5f33c842351a342c0c39c340d27a2af828894fd7
cklwblove <lwbhtml@163.com>
2019-03-21 19:43:16 +0800

    fix: 解决 sidebar 在收起状态时，tooltip 不显示的问题

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -5,15 +5,20 @@
       v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
       <app-link :to="resolvePath(onlyOneChild.path)">
         <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item v-if="onlyOneChild.meta" :icon="onlyOneChild.meta.icon||item.meta.icon"
-                :title="generateTitle(onlyOneChild.meta.title)"/>
+          <template v-if="onlyOneChild.meta">
+            <svg-icon v-if="onlyOneChild.meta.icon" :name="onlyOneChild.meta.icon || item.meta.icon"/>
+            <span slot="title">{{generateTitle(onlyOneChild.meta.title)}}</span>
+          </template>
         </el-menu-item>
       </app-link>
     </template>
 
     <el-submenu v-else ref="submenu" :index="resolvePath(item.path)">
-      <template slot="title">
-        <item v-if="item.meta" :icon="item.meta.icon" :title="generateTitle(item.meta.title)"/>
+      <template v-if="item.meta">
+        <template slot="title">
+          <svg-icon v-if="item.meta.icon" :name="item.meta.icon"/>
+          <span slot="title">{{generateTitle(item.meta.title)}}</span>
+        </template>
       </template>
 
       <template v-for="child in item.children">
@@ -28,7 +33,10 @@
 
           <app-link v-else :to="resolvePath(child.path)" :key="child.name">
             <el-menu-item :index="resolvePath(child.path)">
-              <item v-if="child.meta" :icon="child.meta.icon" :title="generateTitle(child.meta.title)"/>
+              <template v-if="child.meta">
+                <svg-icon v-if="child.meta.icon" :name="child.meta.icon"/>
+                <span slot="title">{{generateTitle(child.meta.title)}}</span>
+              </template>
             </el-menu-item>
           </app-link>
         </template>
@@ -39,76 +47,74 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
-import { Route } from 'vue-router';
-import path from 'path';
-import { isExternal } from '@/utils';
-import Item from './Item.vue';
-import AppLink from './Link.vue';
-import FixiOSBug from './FixiOSBug';
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+  import { Route } from 'vue-router';
+  import path from 'path';
+  import { isExternal } from '@/utils';
+  import AppLink from './Link.vue';
+  import FixiOSBug from './FixiOSBug';
 
-// TODO 侧边栏没有 tooltip 显示
-@Component({
-  components: {
-    Item,
-    AppLink
-  },
-  mixins: [FixiOSBug]
-})
-export default class SidebarItem extends Vue {
-  @Prop({required: true, default: {}}) item!: Route;
-  @Prop({default: false}) isNest!: boolean;
-  @Prop({default: ''}) basePath!: string;
+  // TODO 侧边栏没有 tooltip 显示
+  @Component({
+    components: {
+      AppLink
+    },
+    mixins: [FixiOSBug]
+  })
+  export default class SidebarItem extends Vue {
+    @Prop({required: true, default: {}}) item!: Route;
+    @Prop({default: false}) isNest!: boolean;
+    @Prop({default: ''}) basePath!: string;
 
-  onlyOneChild: null = null;
+    onlyOneChild: null = null;
 
-  hasOneShowingChild(children, parent) {
-    const showingChildren = children.filter((item) => {
-      if (item.hidden) {
-        return false;
-      } else {
-        // Temp set(will be used if only has one showing child)
-        this.onlyOneChild = item;
+    hasOneShowingChild(children, parent) {
+      const showingChildren = children.filter((item) => {
+        if (item.hidden) {
+          return false;
+        } else {
+          // Temp set(will be used if only has one showing child)
+          this.onlyOneChild = item;
+          return true;
+        }
+      });
+
+      // When there is only one child router, the child router is displayed by default
+      if (showingChildren.length === 1) {
         return true;
       }
-    });
 
-    // When there is only one child router, the child router is displayed by default
-    if (showingChildren.length === 1) {
-      return true;
+      // Show parent if there are no child router to display
+      if (showingChildren.length === 0) {
+        this.onlyOneChild = {...parent, path: '', noShowingChildren: true};
+        return true;
+      }
+
+      return false;
     }
 
-    // Show parent if there are no child router to display
-    if (showingChildren.length === 0) {
-      this.onlyOneChild = {...parent, path: '', noShowingChildren: true};
-      return true;
+    resolvePath(routePath) {
+      if (this.isExternalLink(routePath)) {
+        return routePath;
+      }
+      return path.resolve(this.basePath, routePath);
     }
 
-    return false;
-  }
-
-  resolvePath(routePath) {
-    if (this.isExternalLink(routePath)) {
-      return routePath;
+    isExternalLink(routePath) {
+      return isExternal(routePath);
     }
-    return path.resolve(this.basePath, routePath);
-  }
 
-  isExternalLink(routePath) {
-    return isExternal(routePath);
-  }
+    generateTitle(title) {
+      const hasKey = this.$te('route.' + title);
+      // console.log('generateTitle', title);
+      if (hasKey) {
+        // $t :this method from vue-i18n, inject in @/lang/index.js
+        const translatedTitle = this.$t('route.' + title);
 
-  generateTitle(title) {
-    const hasKey = this.$te('route.' + title);
-    // console.log('generateTitle', title);
-    if (hasKey) {
-      // $t :this method from vue-i18n, inject in @/lang/index.js
-      const translatedTitle = this.$t('route.' + title);
-
-      return translatedTitle;
+        return translatedTitle;
+      }
+      return title;
     }
-    return title;
   }
-}
 </script>
 
